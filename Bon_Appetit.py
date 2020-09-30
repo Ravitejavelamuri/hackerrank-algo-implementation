932b0e9bb36a8fda884769ffe5f40f38c2d58119
Ravitejavelamuri <39962669+Ravitejavelamuri@users.noreply.github.com>
2020-10-01 01:06:46 +0530

    Update Bon_Appetit.py

--- Bon_Appetit.py
+++ Bon_Appetit.py
@@ -1,9 +1,9 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
-n, k = map(int, raw_input().split())
-A = map(int, raw_input().split())
-b = int(raw_input())
+n, k = map(int, input().split())
+l = map(int, input().split())
+b = int(input())
 
-tot = sum(A) - A[k]
-if 2*b == tot: print "Bon Appetit"
-else: print b - tot / 2
+tot = sum(l) - l[k]
+if 2*b == tot: print("Bon Appetit")
+else: print(b - tot / 2)
