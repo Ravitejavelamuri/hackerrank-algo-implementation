c8f4874984753c98321f776835dbab9c2c2d176b
Ravitejavelamuri <39962669+Ravitejavelamuri@users.noreply.github.com>
2020-10-01 01:21:30 +0530

    Update bigger_is_greater.py

--- bigger_is_greater.py
+++ bigger_is_greater.py
@@ -1,6 +1,6 @@
 for _ in range(int(input())):
     s = input()
-    has_greater = False
+    has_g = False
     for i in range(len(s)-1)[::-1]:
         if ord(s[i]) < ord(s[i+1]):
             for j in range(i+1,len(s))[::-1]:
@@ -8,10 +8,11 @@ for _ in range(int(input())):
                     lis = list(s)
                     lis[i],lis[j]=lis[j],lis[i]
                     print("".join(lis[:i+1]+lis[i+1:][::-1]))
-                    has_greater = True
-                if has_greater == True:
+                    has_g = True
+                if has_g == True:
                     break
-        if has_greater == True:
-            break
-    if has_greater == False:
-        print("no answer")
+                if has_g == False:
+                    print("no answer")
+                if has_g == True:
+                    break
+   
